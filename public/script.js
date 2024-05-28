0533a7334f85e0a8cf1db7d1ef46195bcde00727
nadavswartz <nadav.swartz@gmail.com>
2024-05-28 13:06:13 +0300

    user button finished
    finished working on the user button now works good, need to implament the buttons themselves

--- public/script.js
+++ public/script.js
@@ -1,16 +1,43 @@
 // Global veribals
 
 const container = document.querySelector(".container"),
+  overlay = document.querySelector(".overlay"),
   pwShowHide = document.querySelectorAll(".showHidePw"),
-  pwFields = document.querySelectorAll(".password")
+  pwFields = document.querySelectorAll(".password");
 
 const user = document.getElementById("user");
+const button = document.getElementById("floating-button");
 
 // Nadav's S. Code
 
-user.addEventListener("click", () => {
-  console.log("it works");
+function showContainer() {
+  overlay.classList.remove("hide");
   container.classList.remove("hide");
+  button.classList.remove("hide");
+  setTimeout(() => {
+    overlay.classList.add("show");
+    container.classList.add("show");
+    button.classList.add("show");
+  }, 100); // Adding a slight delay to ensure the transition takes effect
+}
+
+function hideContainer() {
+  overlay.classList.remove("show");
+  container.classList.remove("show");
+  button.classList.remove("show");
+  setTimeout(() => {
+    overlay.classList.add("hide");
+    container.classList.add("hide");
+    button.classList.add("hide");
+  }, 500);
+}
+
+user.addEventListener("click", () => {
+  setTimeout(showContainer, 300); // Show the container and overlay after 2 seconds
+});
+
+button.addEventListener("click", () => {
+  setTimeout(hideContainer, 300);
 });
 
 //   js code to show/hide password and change icon
