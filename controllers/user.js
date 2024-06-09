140912daeff859eba46f2b82297e6347bda9b601
Nadav <nadavb246@gmail.com>
2024-06-09 17:39:05 +0300

    update user.username for the console.log

--- controllers/user.js
+++ controllers/user.js
@@ -20,7 +20,7 @@ exports.login = async (req, res) => {
         const user = await userService.login(email, password);
         if (user) {
             req.session.userId = user._id;
-            console.log( `user ${email} login `) // for me to see that a user login propartly
+            console.log( `user ${user.username} login `) // for me to see that a user login propartly
             res.redirect('/home');
         } else {
             res.status(401).json({ errors: ['Invalid email or password'] });
